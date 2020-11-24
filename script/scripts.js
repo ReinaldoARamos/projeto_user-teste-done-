84b9d25ce8ea092a1f39491a43fcc069f3cea84d
Reinaldo Ramos <55931337+Selkadoom@users.noreply.github.com>
2020-11-24 18:06:28 -0300

    Recebendo o contructor
    Nesse comit oque foi feito no código foi basicamtne uma total mudança do Script, agora ele recebe a classe UserController e instancia em uma variável,  lançando os ids da tabela e Users, além de chamar a função onSubmit

--- script/scripts.js
+++ script/scripts.js
@@ -1,28 +1,7 @@
-var fields = document.querySelectorAll("#form-user-create [name]");
-var user = {};
+let UserController = new userController("form-user-create", "table-user"); //recebe os ids e cria um novo objeto
 
-function AddLine(dataUser) {
-    console.log(dataUser);
-  var tr =  document.createElement("tr"); //cria a tabela
+UserController.OnSubmit();
 
-  tr.innerHTML =
-  //inserir comanbdos no HTML 
-  ` <tr> 
-  <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
-  <td>${dataUser.name}</td>
-  <td>${dataUser.email}</td>
-  <td>${dataUser.admin}</td>
-  <td>${dataUser.birth}</td>
-  <td>
-    <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
-    <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
-  </td>
-</tr>`
-
- document.getElementById('table-user').appendChild(tr); //pegar a table do HTML
-
-    
-}
-
+//function addLine colocada no Controller
 
 //codigo de submit apagado e colocado dentro do Controller OnSubmit
