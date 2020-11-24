d04e1dfb49dd9c677ffb817065c4bfc8985f1832
Reinaldo Ramos <55931337+Selkadoom@users.noreply.github.com>
2020-11-24 17:53:18 -0300

    Adicionando o método AddLine
    Aqui ele recebeu o método addLine, trocando o parâmetro para TableId para receber o Id da tabela

--- controller/controller.js
+++ controller/controller.js
@@ -56,4 +56,28 @@ this.formEl = document.getElementById(formId)
 
 
     }
+
+    AddLine(dataUser, TableId) {
+      
+  
+    
+      document.getElementById(TableId).innerHTML = //Colocamos o TableId para ele receber o Id da tabela toda
+      //inserir comanbdos no HTML 
+      ` <tr> 
+      <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+      <td>${dataUser.name}</td>
+      <td>${dataUser.email}</td>
+      <td>${dataUser.admin}</td>
+      <td>${dataUser.birth}</td>
+      <td>
+        <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+      </td>
+    </tr>`
+    
+    // document.getElementById('table-user').appendChild(tr); //pegar a table do HTML
+    
+        
+    }
+    
 }
