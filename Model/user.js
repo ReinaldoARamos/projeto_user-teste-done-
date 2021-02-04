ceae4b005fab6c3d2d9f0418704efbad108f0724
Reinaldo Ramos <55931337+Selkadoom@users.noreply.github.com>
2021-02-04 20:30:11 -0300

    04/02 20:26 resolvendo o erro de Duplicação
    Aqui teve bastante coisa errada, para começar, os 3 iguais davam problema, pois queria que fosse idêntico, inclusive no tipo, então foi trocada para 2 == para fazer a comparação. Em seguida, tive que arrumar um erro nas declarações no topo do código, pois o id tinha se misturado com o name. No Save, tive que trocar bastante coisa, ao invés de usar object assing, eu coloquei o user do "u" para se comparar com o id do users, ai depois disso ele iguala os ids, assim substituindo os valores.

--- Model/user.js
+++ Model/user.js
@@ -1,129 +1,114 @@
-class User{
-
-    constructor(name, gender, birth, country, email, password, photo, admin){
-        this._id = 
-          this._name = name;
-          this._gender = gender;
-          this._birth = birth;
-          this._country = country;
-          this._email = email;
-          this._password = password;
-          this._photo = photo;
-          this._admin = admin; 
-          this._register = new Date();
-    } 
-   
-    get id(){
-        return this._id;
-    }
-    get register(){
-        return this._register;
-    }
-
-
-    get name(){
-        return this._name; 
-
-    };
-
-    get gender(){
-        return this._gender
-
-    };
-
-    get birth(){
-        return this._birth
-
-    };
-
-    get country(){
-
-        return this._country
-    };
-
-    get email(){
-        return this._email
-
-    };
-
-    get password(){
-        return this._password
-
-    };
-
-    get photo(){
-        return this._photo
-
-    };
-
-    get admin(){
-
-        return this._admin
-    };
-
-    set photo(value){
-        this._photo = value
-        
-    }
-    loadFromJSON(json){
-        for(let name in json){
-
-            switch (name) {
-                case '_register':
-                    this[name] = new Date(json[name]);
-                    
-                    break;
-            
-                default:
-                    this[name] = json[name]
-            }
-
-
-
-           
-        }
-    }
-
-    getUserStorage() {
-        let users = [];
+class User {
+  constructor(name, gender, birth, country, email, password, photo, admin) {
+    this.id;
+     this._name = name;
+    this._gender = gender;
+    this._birth = birth;
+    this._country = country;
+    this._email = email;
+    this._password = password;
+    this._photo = photo;
+    this._admin = admin;
     
-        if (localStorage.getItem("users")) {
-          users = JSON.parse(localStorage.getItem("users"));
-        }
-        return users;
+    this._register = new Date();
+  }
+
+  get id() {
+    return this._id;
+  }
+  get register() {
+    return this._register;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get gender() {
+    return this._gender;
+  }
+
+  get birth() {
+    return this._birth;
+  }
+
+  get country() {
+    return this._country;
+  }
+
+  get email() {
+    return this._email;
+  }
+
+  get password() {
+    return this._password;
+  }
+
+  get photo() {
+    return this._photo;
+  }
+
+  get admin() {
+    return this._admin;
+  }
+
+  set photo(value) {
+    this._photo = value;
+  }
+
+  /*
+  set id(id) {
+      this._id = id
+  }
+  */
+  loadFromJSON(json) {
+    for (let name in json) {
+      switch (name) {
+        case "_register":
+          this[name] = new Date(json[name]);
+
+          break;
+
+        default:
+          this[name] = json[name];
       }
-
-      getNewId(){
-
-        if (!window.id) window.id = 0; // a partir do igual nós podemos chamar o id sem window, pois ele já está referenciado
-
-        id ++ ; 
-        return id;
-      }
-    save(){
-        let users = this.getUserStorage(); //retorna todos os usuários em um array e faz um parse
-
-        if (this.id > 0){ //verifica se há um id 
-          users.map(u => {
-            if( u._id === this.id){
-
-                u = this; //lembrando que o this é o próprio user
-            }
-
-            return u;
-          });
-
-        } else{
-
-            this._id = this.getNewId();
-            users.push(this);
-    
-          
-
-        } 
-         // sessionStorage.setItem("users", JSON.stringify(users)); //aqui é onde gera a chave e o valor,
-         localStorage.setItem("users", JSON.stringify(users)); //aqui é onde gera a chave e o valor,
-         // sendo o primeiro a chave e o segundo o valor
-
     }
+  }
+
+ static getUserStorage() {
+    let users = [];
+
+    if (localStorage.getItem("users")) {
+      users = JSON.parse(localStorage.getItem("users"));
+    }
+    return users;
+  }
+
+  getNewId() {
+    if (!window.id) window.id = 0; // a partir do igual nós podemos chamar o id sem window, pois ele já está referenciado
+
+    id++;
+    return id;
+  }
+  save() {
+    let users = User.getUserStorage(); //retorna todos os usuários em um array e faz um parse
+
+    if (this.id > 0) {
+      //verifica se há um id
+      users = users.map(u => {
+        if (u._id == this.id) {
+            u = this;
+        }
+
+        return u;
+ });
+    } else {
+      this._id = this.getNewId();
+      users.push(this);
+    }
+
+    // sessionStorage.setItem("users", JSON.stringify(users)); //aqui é onde gera a chave e o valor,
+    localStorage.setItem("users", JSON.stringify(users)); //aqui é onde gera a chave e o valor,
+    // sendo o primeiro a chave e o segundo o valor
+  }
 }
